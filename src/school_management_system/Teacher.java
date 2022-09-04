e669ebc59db3c4bc3c09f91f71c4ea2eba1afd95
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-04 17:41:33 -0400

    Declaring properties "Field names"

--- src/school_management_system/Teacher.java
+++ src/school_management_system/Teacher.java
@@ -1,4 +1,10 @@
 package school_management_system;
 
+/**
+ * This class is responsable for keeping the track of teacher's name, id, salary
+ */
 public class Teacher {
+    private int id;
+    private String name;
+    private int salary;
 }
