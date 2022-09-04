969e528eb3188d285d5d78df6b73726746d1c62e
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-04 17:01:43 -0400

    Generate Constructor by creating a new student by initializing

--- src/school_management_system/Student.java
+++ src/school_management_system/Student.java
@@ -10,4 +10,19 @@ public class Student {
     private int feesPaid;
     private int feesTotal;
 
+    /**
+     * To create a new student by initializing
+     * Fees for every student is 30.000
+     * Fees paid initially is 0
+     * @param id id for the student: unique
+     * @param name name of the student
+     * @param grade grade of the student
+     */
+    public Student(int id, String name, int grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+        this.feesPaid = 0;
+        this.feesTotal = 30000;
+    }
 }
