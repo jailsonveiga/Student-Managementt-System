0010e7be718f15d17b8c32d26939626feb42f161
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-04 16:51:06 -0400

    Declaring properties "fields"

--- src/school_management_system/Student.java
+++ src/school_management_system/Student.java
@@ -1,5 +1,13 @@
 package school_management_system;
 
+/**
+ * This class is responsible for keeping the track of students fees, name, grades & fees paid
+ */
 public class Student {
-///
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+
 }
