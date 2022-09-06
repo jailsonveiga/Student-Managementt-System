73852f7a83b8c1623f0ae79829a84a2eba874c7d
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-05 22:06:24 -0400

    Declaring properties "fields"

--- src/school_management_system/School.java
+++ src/school_management_system/School.java
@@ -1,4 +1,18 @@
 package school_management_system;
 
+import java.util.List;
+
+/**
+ * Many teachers, many students
+ * Implements teachers and student
+ * using an array list
+*
+ */
+
 public class School {
+
+    private List<Teacher> teachers;
+    private List<Student> students;
+    private int totalMoneyEarned;
+    private int totalMoneySpent;
 }
