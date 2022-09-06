659c18cb3e87daa63de5b2dadac5d156aa61bfd4
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-05 22:22:20 -0400

    Generate Getter and Setter

--- src/school_management_system/School.java
+++ src/school_management_system/School.java
@@ -27,4 +27,67 @@ public class School {
         totalMoneyEarned = 0;
         totalMoneySpent = 0;
     }
+
+    /**
+     * @return the list of teachers in the school
+     */
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+
+    /**
+     * Adds a teacher to the school
+     * @param teacher the teacher to be added
+     */
+    public void addTeacher(Teacher teacher) {
+        this.teachers.add(teacher);
+    }
+
+    /**
+     * @return the list of students in the school
+     */
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    /**
+     * Adds a student to the school
+     * @param student the student to be added
+     */
+    public void addStudent(Student student) {
+        this.students.add(student);
+    }
+
+    /**
+     * @return the total money earned by the school
+     */
+    public int getTotalMoneyEarned() {
+        return totalMoneyEarned;
+    }
+
+    /**
+     * Adds the total money earned by the school
+     * @param moneyEarned money that is supposed to be added
+     */
+    public void updateTotalMoneyEarned(int moneyEarned) {
+        this.totalMoneyEarned += moneyEarned;
+    }
+
+    /**
+     *
+     * @return the total money spent by the school
+     */
+    public int getTotalMoneySpent() {
+        return totalMoneySpent;
+    }
+
+    /**
+     * update the money that is spent by the school which
+     * is the salary given by the school to its teachers
+     * @param moneySpent the money spent by school
+     */
+    public void updateTotalMoneySpent(int moneySpent) {
+        this.totalMoneySpent -= moneySpent;
+    }
 }
