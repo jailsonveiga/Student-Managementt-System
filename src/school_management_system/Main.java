bdfd19dd433b37a3f00c10d43d7338efcc096fce
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-05 22:27:46 -0400

    Create new Object Instance of Teachers

--- src/school_management_system/Main.java
+++ src/school_management_system/Main.java
@@ -3,5 +3,9 @@ package school_management_system;
 public class Main {
     public static void main(String[] args) {
 
+        // Create new Object instance of teachers
+        Teacher lizzy = new Teacher(1, "Lizzy", 500);
+        Teacher mellisa = new Teacher(2, "Mellisa", 700);
+        Teacher vanderhorn = new Teacher(3, "Vanderhorn", 600);
     }
 }
