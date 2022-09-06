f2efca22bf48fd470a42a9b314aa161097a2f412
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-05 23:24:59 -0400

    Override toString

--- src/school_management_system/Student.java
+++ src/school_management_system/Student.java
@@ -90,4 +90,9 @@ public class Student {
     public int getRemainingFees() {
         return feesTotal - feesPaid;
     }
+
+    @Override
+    public String toString() {
+        return "Student's name: " + name + " Total fees paid so far $" + feesPaid;
+    }
 }
